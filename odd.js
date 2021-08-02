5a2151cf1ecdca5aa39cb66d20d443cb0a9cac95
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-08-02 16:26:48 -0700

    Compass challenge : Odd Checker

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd  = function (num) {
+  return num % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("0 is odd: " + isOdd(0));
+console.log("-4 is odd: " + isOdd(-4));
+console.log("-3 is odd: " + isOdd(-3));
